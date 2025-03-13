39cc1b2b9736f26de2c4717428a4af65bb231a83
Khadija Khalid <khadija.khalid192@gmail.com>
2025-03-13 11:26:19 +0500

    modified db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/event-planner")
+  .connect("mongodb://127.0.0.1:27017/event-planner")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
